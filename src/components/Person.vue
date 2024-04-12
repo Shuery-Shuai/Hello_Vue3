cf631cca948bea4bccce5a09d3421406f6518d80
Shuery <2463253700@qq.com>
2024-04-12 17:38:22 +0800

    setup() 语法糖。

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,18 +2,19 @@
   <div class="person">
     <h2>姓名：{{ name }}</h2>
     <h2>年龄：{{ age }}</h2>
+    <h2>地址：{{ address }}</h2>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
     <button @click="showTel">查看联系方式</button>
-    <!-- <hr /> -->
-    <!-- <h2>测试 1：{{ a }}</h2> -->
-    <!-- <h2>测试 2：{{ c }}</h2> -->
-    <!-- <h2>测试 3：{{ d }}</h2> -->
-    <!-- <button @click="b">测试</button> -->
+    <!-- <hr />
+    <h2>测试 1：{{ a }}</h2>
+    <h2>测试 2：{{ c }}</h2>
+    <h2>测试 3：{{ d }}</h2>
+    <button @click="b">测试</button> -->
   </div>
 </template>
 
-<script lang="ts">
+<!-- <script lang="ts">
 export default {
   name: "Person",
   beforeCreate() {
@@ -34,24 +35,6 @@ export default {
   // },
   setup() {
     // setup() 中的 this 是 undefined，Vue3 中已经弱化了 this。
-    // 数据，原本写在 data() 中，此时的 name、age、tel 都非响应式数据。
-    let name = "张三";
-    let age = 18;
-    let tel = "12345678901";
-
-    // 方法
-    function changeName() {
-      name = "张三"; // 注意：这样修改 name 页面不会发生变化。
-      console.log(name); // name 确实已更改，但却并非响应式。
-    }
-
-    function changeAge() {
-      age += 1; // 注意：这样修改 age 页面不会发生变化。
-      console.log(age); // age 确实已更改，但却并非响应式。
-    }
-    function showTel() {
-      alert(tel);
-    }
 
     // 将数据、方法返回，模板中才可以使用。
     return { name, age, changeName, changeAge, showTel };
@@ -60,8 +43,29 @@ export default {
     // return () => "23333333!!!";
   },
 };
-</script>
+</script> -->
 
+<script lang="ts" setup name="Person">
+// 数据，原本写在 data() 中，此时的 name、age、tel 都非响应式数据。
+let name = "张三";
+let age = 18;
+let tel = "12345678901";
+let address = "北京市朝阳社区";
+
+// 方法
+function changeName() {
+  name = "张三"; // 注意：这样修改 name 页面不会发生变化。
+  console.log(name); // name 确实已更改，但却并非响应式。
+}
+
+function changeAge() {
+  age += 1; // 注意：这样修改 age 页面不会发生变化。
+  console.log(age); // age 确实已更改，但却并非响应式。
+}
+function showTel() {
+  alert(tel);
+}
+</script>
 <style scoped>
 .person {
   background-color: beige;
