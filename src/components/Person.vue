920c4a27010c688280b0b6f4844cad7450424c53
Shuery <2463253700@qq.com>
2024-04-13 15:27:46 +0800

    computed() 计算属性。

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -12,12 +12,17 @@
     <h2>测试 2：{{ c }}</h2>
     <h2>测试 3：{{ d }}</h2>
     <button @click="b">测试</button> -->
-    <h2>姓名：{{ person.name }}</h2>
+    <!-- <h2>姓名：{{ person.name }}</h2>
     <h2>年龄：{{ person.age }}</h2>
     <h2>电话：{{ person.tel }}</h2>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
-    <button @click="changeTel">修改电话</button>
+    <button @click="changeTel">修改电话</button> -->
+    名：<input type="text" v-model="firstName" /><br />
+    姓：<input type="text" v-model="lastName" /><br />
+    全名：<span>{{ fullName }}</span
+    ><br />
+    <button @click="changeFullName">修改名字为 Si-Li</button>
   </div>
 </template>
 
@@ -74,25 +79,62 @@ export default {
 // function showTel() {
 //   alert(tel);
 // }
-import { reactive, toRefs, toRef } from "vue";
+// import { reactive, toRefs, toRef } from "vue";
 
-let person = reactive({ name: "张三", age: 18, tel: "12345678901" });
+// let person = reactive({ name: "张三", age: 18, tel: "12345678901" });
 
-// 利用 toRefs() 解构响应式数据，即将 reactive() 对象数据中的每一键值赋予另一新定义 ref() 变量并将其与原本数据中的键值绑定，类似于指针。
-let { name, age } = toRefs(person);
-// toRef() 则单独处理某一键值。
-let tel = toRef(person, "tel");
+// // 利用 toRefs() 解构响应式数据，即将 reactive() 对象数据中的每一键值赋予另一新定义 ref() 变量并将其与原本数据中的键值绑定，类似于指针。
+// let { name, age } = toRefs(person);
+// // toRef() 则单独处理某一键值。
+// let tel = toRef(person, "tel");
 
-function changeName() {
-  name.value += ".";
-  console.log(name.value, person.name);
-}
-function changeAge() {
-  age.value += 1;
-}
-function changeTel() {
-  tel.value = "14785236903";
-  console.log(tel.value, person.tel);
+// function changeName() {
+//   name.value += ".";
+//   console.log(name.value, person.name);
+// }
+// function changeAge() {
+//   age.value += 1;
+// }
+// function changeTel() {
+//   tel.value = "14785236903";
+//   console.log(tel.value, person.tel);
+// }
+import { computed, ref } from "vue";
+
+let firstName = ref("San");
+let lastName = ref("Zhang");
+
+// 这么定义的 fullName 为计算属性，且只读。
+// let fullName = computed(() => {
+//   return (
+//     firstName.value.slice(0, 1).toUpperCase() +
+//     firstName.value.slice(1) +
+//     "-" +
+//     lastName.value.slice(0, 1).toUpperCase() +
+//     lastName.value.slice(1)
+//   );
+// });
+
+// 这么定义的 fullName 为计算属性，可读可写。
+let fullName = computed({
+  get() {
+    return (
+      firstName.value.slice(0, 1).toUpperCase() +
+      firstName.value.slice(1) +
+      "-" +
+      lastName.value.slice(0, 1).toUpperCase() +
+      lastName.value.slice(1)
+    );
+  },
+  set(val) {
+    const [str1, str2] = val.split("-");
+    firstName.value = str1;
+    lastName.value = str2;
+  },
+});
+
+function changeFullName() {
+  fullName.value = "li-si";
 }
 </script>
 <style scoped>
