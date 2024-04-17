d041209eac27e6c1252bd2778b9841db675a0150
Shuery <2463253700@qq.com>
2024-04-17 20:10:50 +0800

    生命周期。

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,9 @@
       <button @click="showLog">点我输出 H3 元素</button>
     <button @click="test">测试</button> -->
     <!-- <TypeScript /> -->
-    <Props a="年龄" :list="personList" />
+    <!-- <Props a="年龄" :list="personList" /> -->
+    <LifeCircle v-if="isShow" />
+    <button @click="showNot">{{ mountNot }}</button>
     <!-- <Person />
     <Car />
     <Game /> -->
@@ -33,7 +35,7 @@
 
 <script lang="ts" setup>
 // JS 或 TS
-import { reactive, ref } from "vue";
+import { reactive, ref, onMounted } from "vue";
 import { type Persons } from "@/types";
 import SimpleProject from "./components/SimpleProject.vue";
 import OptionsAPI from "./components/OptionsAPI.vue";
@@ -55,6 +57,7 @@ import WatchEffect from "./components/WatchEffect.vue";
 import TagRef from "./components/TagRef.vue";
 import TypeScript from "./components/TypeScript.vue";
 import Props from "./components/Props.vue";
+import LifeCircle from "./components/LifeCircle.vue";
 import Person from "./components/Person.vue";
 import Car from "./components/Car.vue";
 import Game from "./components/Game.vue";
@@ -66,6 +69,8 @@ let personList = reactive<Persons>([
   { id: "lisi", name: "李四", age: 50 },
   { id: "wangwu", name: "王五", age: 40 },
 ]);
+let isShow = ref(true);
+let mountNot = ref("卸载");
 
 function showLog() {
   console.log(title3.value);
@@ -73,6 +78,18 @@ function showLog() {
 function test() {
   console.log(tagref.value);
 }
+function showNot() {
+  isShow.value = !isShow.value;
+  if (mountNot.value == "卸载") {
+    mountNot.value = "挂载";
+  } else if (mountNot.value == "挂载") {
+    mountNot.value = "卸载";
+  }
+}
+
+onMounted(() => {
+  console.log("父——挂载完毕");
+});
 </script>
 
 <style>
---
+++ src/components/LifeCircle.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="sum">
+    <h1>生命周期</h1>
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="add">点我 SUM 加一</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  ref,
+} from "vue";
+
+let sum = ref(0);
+
+function add() {
+  sum.value++;
+}
+
+// 创建
+console.log("创建");
+
+// 挂载
+onBeforeMount(() => {
+  console.log("挂载之前");
+});
+onMounted(() => {
+  console.log("子——挂载完毕");
+});
+
+// 更新
+onBeforeUpdate(() => {
+  console.log("更新之前");
+});
+onUpdated(() => {
+  console.log("更新完毕");
+});
+
+// 卸载
+onBeforeUnmount(() => {
+  console.log("卸载之前");
+});
+onUnmounted(() => {
+  console.log("卸载完毕");
+});
+</script>
+
+<style scoped>
+.sum {
+  background-color: wheat;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
