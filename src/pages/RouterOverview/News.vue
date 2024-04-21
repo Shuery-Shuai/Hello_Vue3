914583b72c8a5c458e5f696b16d75a52eafed9ff
Shuery <2463253700@qq.com>
2024-04-21 19:40:03 +0800

    编程式路由导航。。

--- src/pages/RouterOverview/News.vue
+++ src/pages/RouterOverview/News.vue
@@ -7,9 +7,11 @@
         <!-- <RouterLink
           :to="`/news/detail?id=${news.id}&title=${news.title}&content=${news.content}`"
           >{{ news.title }}</RouterLink
-        > -->
+          > -->
         <!-- 第二种写法 -->
+        <!-- replace 模式不能够返回 -->
         <RouterLink
+          replace
           :to="{
             name: 'detail',
             query: {
@@ -22,15 +24,16 @@
         >
         <!-- 第三种写法 -->
         <!-- <RouterLink
-          :to="{
-            // 在 to 对象中使用 params 不能够使用 path
-            // path: '/news/detail',
-            name: 'detail',
-            // params 无法传递数组
-            params: { id: news.id, title: news.title, content: news.content },
-          }"
-          >{{ news.title }}</RouterLink
-        > -->
+            :to="{
+              // 在 to 对象中使用 params 不能够使用 path
+              // path: '/news/detail',
+              name: 'detail',
+              // params 无法传递数组
+              params: { id: news.id, title: news.title, content: news.content },
+            }"
+            >{{ news.title }}</RouterLink
+            > -->
+        <button @click="showNewsDetail(news)">查看新闻</button>
       </li>
     </ul>
     <!-- 展示区 -->
@@ -42,6 +45,7 @@
 
 <script setup lang="ts">
 import { reactive } from "vue";
+import { useRouter } from "vue-router";
 
 const newsList = reactive([
   { id: "01", title: "很好的抗癌食物", content: "西兰花" },
@@ -49,6 +53,23 @@ const newsList = reactive([
   { id: "03", title: "震惊，万万没想到", content: "明天是周一" },
   { id: "04", title: "好消息！", content: "快过年了" },
 ]);
+const router = useRouter();
+
+interface NewsInter {
+  id: string;
+  title: string;
+  content: string;
+}
+function showNewsDetail(news: NewsInter) {
+  router.replace({
+    name: "detail",
+    query: {
+      id: news.id,
+      title: news.title,
+      content: news.content,
+    },
+  });
+}
 </script>
 
 <style scoped>
